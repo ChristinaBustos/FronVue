8b6a4b849dcbcdb6634cc45f3e9fde7493f13efe
ChristinaBustos <105693387+ChristinaBustos@users.noreply.github.com>
2023-05-16 23:35:32 -0600

    completo

--- src/components/Top-Nav.vue
+++ src/components/Top-Nav.vue
@@ -1,22 +1,27 @@
 <template>
-    <div>
-        <b-navbar toggleable="lg" type="dark" variant="dark">
-            <b-navbar-brand href="#">NavBar</b-navbar-brand>
+   <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand href="#">
+        <img src="./../assets/Logo-utez.png" width="100" >
+       <b>CRUD</b> 
+    </b-navbar-brand>
 
-            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-            <b-collapse id="nav-collapse" is-nav>
-                <b-navbar-nav>
-                    <b-nav-item href="#">Personas</b-nav-item>
-                </b-navbar-nav>
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item href="#">Usuarios</b-nav-item>
+      </b-navbar-nav>
 
-                <!-- Right aligned nav items -->
-                <b-navbar-nav class="ml-auto">
-                    
-                </b-navbar-nav>
-            </b-collapse>
-        </b-navbar>
-    </div>
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-form>
+          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+        </b-nav-form>
+
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
 </template>
 
 <script lang="ts">
--- src/modules/person/adapters/view/PeopleView.vue
+++ src/modules/person/adapters/view/PeopleView.vue
@@ -2,11 +2,12 @@
     <div>
         <Topnav />
         <div class="mx-5 mt-5">
-            <div class="d-flex align-items-center">
-                <h1>Gestión de personal</h1>
-                <button v-b-modal.modal-1 class="btn btn-success ml-auto">Añadir</button>
-                <button v-b-modal.modal-5 class="btn btn-danger ml-auto">Exportar PDF</button>
-                <button v-b-modal.modal-3 class="btn btn-success ml-auto">Ingresar Excel</button>
+            <div class="text-center">
+                <h1>Gestión de Usuarios</h1>
+                <button v-b-modal.modal-1 class="btn btn-secondary ml-auto mx-2">Añadir</button>
+                <button class="btn btn-danger m-1" @click="generatePdf">Exportar PDF</button>
+                <button  class="btn btn-info ml-auto mx-2" @click="generateExcel" >Exportar Excel</button>
+                <button v-b-modal.modal-3 class="btn btn-success ml-auto mx-2">Ingresar Excel</button>
             </div>
             <hr>
             <b-table striped hover :items="people" :fields="fields" id="peoplegetall" >
@@ -19,7 +20,6 @@
         <AddModal @findAll="findAll"/>
         <UpdateModal :personSelected="person" @findAll="findAll"/>
         <DeleteModal :personSelected="person" @findAll="findAll" />
-        <CreatePdfModal @findAll="findAll" />
         <ImportExcelModal @findAll="findAll" />
     </div>
 </template>
@@ -30,15 +30,20 @@ import Topnav from '../../../../components/Top-Nav.vue';
 import AddModal from './AddPerson.modal.vue';
 import UpdateModal from './UpdatePerson.modal.vue';
 import DeleteModal from './DeletePerson.modal.vue';
-import CreatePdfModal from './CreatePdf.modal.vue';
 import ImportExcelModal from './ImportExcel.modal.vue';
 import { PersonController } from '../people.controller';
 import { Person } from '../../entities/person';
+import * as XLSX from 'xlsx';
+import pdfMake from 'pdfmake/build/pdfmake.js';
+import pdfFonts from 'pdfmake/build/vfs_fonts.js';
+import { SavePersonDto } from '../dto/save-person';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
 
 export default Vue.extend({
     name: 'PeopleView',
     components: {
-        Topnav, AddModal, UpdateModal,DeleteModal,CreatePdfModal,ImportExcelModal
+        Topnav, AddModal, UpdateModal,DeleteModal,ImportExcelModal
     },
     data() {
         return {
@@ -49,7 +54,8 @@ export default Vue.extend({
                 {key: 'actions', label: 'Acciones'}
             ],
             people: [] as Person[],
-            person: {} as Person
+            person: {} as Person,
+            document: null as any
         }
     },
     methods: {
@@ -62,10 +68,73 @@ export default Vue.extend({
             const controller = new PersonController();
             const response = await controller.findPerson(payload);
             this.person = response.entity!;
+        },
+        generatePdf() {
+            this.document = {
+                content: [
+                    {
+                        style: "tableExample",
+                        table: {
+                            widths: [
+                                '*', '*', '*', '*'
+                            ],
+                            body: [["Nombre", "Apellido Materno", "Apellido Paterno"]],
+                        },
+                    },
+                ],
+                styles: {
+                    tableExample: {
+                        margin: [0, 5, 0, 15],
+                    }
+                }
+            }
+            for (let index = 0; index < this.people.length; index++) {
+                const element = this.people[index];
+
+                const person: SavePersonDto = {
+                    name: element.name,
+                    surname: element.surname,
+                    lastname: element.lastname,
+                }
+                this.document.content[0].table.body.push([
+                    { text: `${person.name}` },
+                    { text: `${person.surname}` },
+                    { text: `${person.lastname}` },
+                ]);
+            }
+            console.table(this.document)
+
+            pdfMake.createPdf(this.document).download(`Reporte_${(new Date()).toString().replace(' ', '-')}`)
+            this.document = null
+        },
+        generateExcel() {
+            const aoa = [] as any
+            this.people.map((person) => {
+                aoa.push([`${person.name}`, `${person.surname}`, `${person.lastname}`])
+            })
+            const worksheet = XLSX.utils.aoa_to_sheet(aoa);
+            XLSX.utils.sheet_add_aoa(worksheet, [["Nombre", "Apellido 1", "Apellido 2"]], { origin: "A1" });
+            const workbook = XLSX.utils.book_new();
+            XLSX.utils.book_append_sheet(workbook, worksheet, "Libro1");
+            const excelBuffer = XLSX.write(workbook, {
+                type: "buffer",
+                bookType: "xlsx",
+            });
+            const blob = new Blob([excelBuffer], {
+                type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+            });
+
+            const url = URL.createObjectURL(blob);
+            const link = document.createElement("a");
+            link.href = url;
+            link.download = `Reporte_${(new Date()).toString().replace(' ','-')}.xlsx`;
+            link.click();
+
+            URL.revokeObjectURL(url)
         }
     },
-    mounted () {
+    mounted() {
         this.findAll();
-    } 
+    }
 });
 </script>
